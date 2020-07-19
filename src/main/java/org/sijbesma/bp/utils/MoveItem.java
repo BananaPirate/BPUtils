654d701e52a65a52bda818b9c3e990854a3c73ad
BananaPirate <janjoost@sijbesma.org>
2020-07-19 21:39:54 +0200

    added slot to slot and fixed an item dupe bug

--- src/main/java/org/sijbesma/bp/utils/MoveItem.java
+++ src/main/java/org/sijbesma/bp/utils/MoveItem.java
@@ -6,24 +6,58 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class MoveItem {
-	
+
 	public static void cursorToSlot(Player player, Inventory inventory, int slot) {
 		ItemStack itemOnCursor = player.getItemOnCursor().clone();
 		ItemStack itemInSlot = inventory.getItem(slot).clone();
-		
-		if(itemOnCursor.isSimilar(itemInSlot)) {
-			//add items from cursor to stack
-			addItemFromCursor(player,inventory,slot);
+
+		if (itemOnCursor.isSimilar(itemInSlot)) {
+			// add items from cursor to stack
+			addItemFromCursor(player, inventory, slot);
 		} else {
-			//swap item and cursor
+			// swap item and cursor
 			swapItemFromCursor(player, inventory, slot);
 		}
 	}
-	
-	public static void ItemStacktoSlot(ItemStack item, Inventory inventory, int slot) {
+
+	public static void itemStacktoSlot(ItemStack item, Inventory inventory, int slot) {
 		inventory.setItem(slot, item.clone());
 	}
-	
+
+	public static boolean slotToSlot(int sourceSlot, int targetSlot, Inventory inventory) {
+		ItemStack sourceStack = inventory.getItem(sourceSlot);
+		ItemStack targetStack = inventory.getItem(targetSlot);
+		if (sourceStack == null) {
+			//cannot move nothing
+			return false;
+		}
+		if (targetStack == null) {
+			// target stack is empty
+			inventory.setItem(targetSlot, sourceStack);
+			inventory.setItem(sourceSlot, new ItemStack(Material.AIR));
+			return true;
+		}
+
+		int sourceAmount = sourceStack.getAmount();
+		int targetAmount = targetStack.getAmount();
+		int targetMax = targetStack.getMaxStackSize();
+		int targetSpace = targetMax - targetAmount;
+
+		if (sourceStack.isSimilar(targetStack)) {
+			if (targetSpace >= sourceAmount) {
+				sourceStack.setAmount(0);
+				targetStack.setAmount(targetAmount + sourceAmount);
+				return true;
+			} else {
+				sourceStack.setAmount(sourceAmount - targetSpace);
+				targetStack.setAmount(targetMax);
+				return true;
+			}
+
+		}
+		//items do not match so do nothing
+		return false;
+	}
 
 	private static void addItemFromCursor(Player player, Inventory inventory, int slot) {
 		ItemStack itemOnCursor = player.getItemOnCursor().clone();
@@ -31,10 +65,9 @@ public class MoveItem {
 		int cursorAmount = itemOnCursor.getAmount();
 		int slotAmount = itemInSlot.getAmount();
 		int spaceInSlot = itemInSlot.getMaxStackSize() - itemInSlot.getAmount();
-		if(spaceInSlot > 0) {
-			if(spaceInSlot >= cursorAmount) {
+		if (spaceInSlot > 0) {
+			if (spaceInSlot >= cursorAmount) {
 				itemInSlot.setAmount(slotAmount + cursorAmount);
-				itemOnCursor.setType(Material.AIR);
 				itemOnCursor.setAmount(0);
 			} else {
 				itemInSlot.setAmount(slotAmount + spaceInSlot);
@@ -42,9 +75,9 @@ public class MoveItem {
 			}
 		}
 		player.setItemOnCursor(itemOnCursor);
-		inventory.setItem(slot, itemInSlot);	
+		inventory.setItem(slot, itemInSlot);
 	}
-	 
+
 	private static void swapItemFromCursor(Player player, Inventory inventory, int slot) {
 		ItemStack itemOnCursor = player.getItemOnCursor().clone();
 		ItemStack itemInSlot = inventory.getItem(slot).clone();
